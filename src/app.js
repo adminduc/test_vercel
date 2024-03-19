02a2eb7c8fbcf610b6a644837cb46e732b2ec81c
MinhDuc <ogdp.vn@gmail.com>
2024-03-19 20:08:10 +0700

    hey

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import * as dotenv from "dotenv";
+// import * as dotenv from "dotenv";
 import * as path from "path";
-import { fileURLToPath } from "url";
-import connectDB from "./configs/database.js";
-import Router from "./routes/index.js";
+// import { fileURLToPath } from "url";
+// import connectDB from "./configs/database.js";
+// import Router from "./routes/index.js";
 const app = express();
-dotenv.config();
+// dotenv.config();
 
-const { PORT, MONGO_URI } = process.env;
+// const { PORT, MONGO_URI } = process.env;
 // Khởi tạo kết nối với cơ sở dữ liệu
-connectDB(MONGO_URI);
+// connectDB(MONGO_URI);
 
 app.use(express.json());
 // Bỏ block fetch api CORS
@@ -19,28 +19,31 @@ app.use(cors());
 app.use(morgan("tiny"));
 
 // Theme home
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 app.get("/", (req, res) => {
   // docs https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+  return res.status(200).json({
+    message: "Hi Server",
+  });
   return res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 // Navigate Router
-app.use("/api", Router);
+// app.use("/api", Router);
 
-// Notfound api
-app.use((req, res, next) => {
-  //   const error = new Error("API not found");
-  //   error.status = 404;
-  //   next(error);
-  return res.status(404).json({
-    message: "API không tồn tại, bỏ cái thói rình mò API người khác đi",
-  });
-});
+// // Notfound api
+// app.use((req, res, next) => {
+//   //   const error = new Error("API not found");
+//   //   error.status = 404;
+//   //   next(error);
+//   return res.status(404).json({
+//     message: "API không tồn tại, bỏ cái thói rình mò API người khác đi",
+//   });
+// });
 
 // Required listening Express server
-app.listen(PORT, (req, res) =>
-  console.log("Listen server running port " + PORT)
+app.listen(8080, (req, res) =>
+  console.log("Listen server running port " + 8080)
 );
 
 export const viteNodeApp = app;
