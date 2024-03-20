0970dd290a2979cdccce52c2cdad41a8f2256194
MinhDuc <ogdp.vn@gmail.com>
2024-03-20 11:19:38 +0700

    ds

--- api/app.js
+++ api/app.js
@@ -1,14 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 // Khởi tạo ứng dụng Express
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`Hello, World! ${process.env.PORT}`); // Gửi "Hello, World!" về cho client
+  res.send(`Hello, World! 3000`); // Gửi "Hello, World!" về cho client
 });
 
 // Lắng nghe các kết nối đến cổng đã chỉ định
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000`);
 });
