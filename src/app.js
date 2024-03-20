d3c6d4584732a64bdc4b32db4089fa455a39c451
MinhDuc <ogdp.vn@gmail.com>
2024-03-20 13:13:03 +0700

    mongoose

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./configs/database.js";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
+app.use(morgan("tiny"));
 
 connectDB(process.env.MONGO_URI);
 app.get("/", (req, res) => res.send(`Express on Vercel ${process.env.PORT}`));
