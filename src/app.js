468d0721258e3f69b4822eb02d20e58957bbe480
MinhDuc <ogdp.vn@gmail.com>
2024-03-20 13:03:02 +0700

    env

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send(`Express on Vercel ${process.env.PORT}`));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(process.env.PORT, () =>
+  console.log(`Server ready on port ${process.env.PORT}`)
+);
 
 export default app;
